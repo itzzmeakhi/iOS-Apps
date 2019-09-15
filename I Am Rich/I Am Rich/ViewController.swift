f79741ec59adab6e21d1f8c3aa0ed03b7746f350
Mallidi Akhil Reddy <akhilmallidi.98@gmail.com>
2019-09-15 08:54:05 +0530

    First Commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Akhil Reddy on 08/09/19.
+//  Copyright © 2019 Akhil Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
