3ed236639962c025c3754d2d8c056697a695be88
Mallidi Akhil Reddy <akhilmallidi.98@gmail.com>
2019-09-15 08:56:07 +0530

    First Commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Akhil Reddy on 10/09/19.
+//  Copyright © 2019 Akhil Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
